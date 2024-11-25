58b7ee4f0fb20cd84255e2cdced040e62b29de43
stan507 <juan.henriquez2101@alumnos.ubiobio.cl>
2024-11-25 20:25:28 -0300

    Cambios menores

--- frontend/src/pages/Admin_local.jsx
+++ frontend/src/pages/Admin_local.jsx
@@ -5,15 +5,40 @@ import Search from '../components/Search';
 /*hooks */
 import useEditProducto from '@hooks/productos/useEditProducto.jsx'
 import useProducto from '@hooks/productos/useGetProductos.jsx'
-import useCreateProducto from '@hooks/productos/useCreateProducto';
+import useCreateProducto from '../hooks/productos/useCreateProducto';
 import useDeleteProducto from '../hooks/productos/useDeleteProducto';
 /*Assets*/
 
 import check from '../assets/check.svg';
+import delete_icon from '../assets/deleteIcon.svg';
+import update_icon from '../assets/updateIcon.svg'
 
 const Admin_local = () => {
+
     const { fetchProductos, setProductos } = useProducto();
-    const [filterId, setFilterId] = useState('');
+    const [filternombre, setFilternombre] = useState('');
+    const [showPopup, setShowPopup] = useState(false);
+    const [newProduct, setNewProduct] = useState({
+        nombre: '',
+        valor: '',
+        stock: ''
+    });
+
+    const togglePopup = () => { setShowPopup(!showPopup); };
+
+    const { handleCreate } = useCreateProducto(fetchProductos);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setNewProduct({ ...newProduct, [name]: value });
+    };
+    {/*Jp del futuro mata todo lo relacionado al subir y lo recreas para entender que hiciste*/ }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await handleCreate(newProduct);
+        setNewProduct({ nombre: '', valor: '', stock: '' });
+        togglePopup();
+    };
 
     const {
         handleClickUpdate,
@@ -24,6 +49,7 @@ const Admin_local = () => {
     } = useEditProducto(setProductos);
 
     const { productos } = useProducto();
+
     const opcionesP = productos.map(producto => ({
         id: producto[0].id,
         nombre: producto[0].nombre,
@@ -34,8 +60,8 @@ const Admin_local = () => {
     const { handleDelete } = useDeleteProducto(fetchProductos, setDataProducto);
 
     const handleIdFilterChange = (e) => {
-        setFilterId(e.target.value);
-      };
+        setFilternombre(e.target.value);
+    };
 
 
     const handleSelectionChange = useCallback((selectedProductos) => {
@@ -44,9 +70,9 @@ const Admin_local = () => {
 
     const columns = [
         { title: "ID", field: "id", width: 100, responsive: 0 },
-        { title: "Nombre", field: "nombre", width: 100, responsive: 3 },
-        { title: "Valor", field: "valor", width: 100, responsive: 2 },
-        { title: "Stock", field: "stock", width: 100, responsive: 2 }
+        { title: "Nombre", field: "nombre", width: 200, responsive: 3 },
+        { title: "Valor", field: "valor", width: 120, responsive: 2 },
+        { title: "Stock", field: "stock", width: 120, responsive: 2 }
     ];
 
     return (
@@ -57,7 +83,7 @@ const Admin_local = () => {
                         Administracion de local
                     </h1>
                     <h2 className="text-2xl font-light text-[#212121]">
-                        Elige el recurso a gestionar
+                        Elija una opcion
                     </h2>
                 </div>
 
@@ -65,27 +91,130 @@ const Admin_local = () => {
 
                     <div className="w-full max-w-5xl bg-white p-8 rounded-xl shadow-lg space-y-6">
 
-                        <div className="flex flex-col items-start justify-between ">
-                            {/*Boton para agregar productos o eliminar prodcutos*/}
+                        <div className="flex flex-col items-end justify-between ">
+                            <div className="flex space-x-4 items-center">
+                                {/*
+                                    Agregar la barra de busquede,recuerda ajustar el largo de esto, ademas
+                                    validar que sean solo misnisculas en el backend
+                                    */}
+                                <Search value={filternombre} onChange={handleIdFilterChange} placeholder={"Filtrar por Nombre"} />
 
-                            <button className="flex flex-col items-center px-4 py-2 bg-purple-500 text-white rounded space-x-2">
-                                <img src={check} alt="edit" />
+                            </div>
+                        </div>
 
-                                <span>Gestionar Productos</span>
-                            </button>
-                            {/*Boton para agregar el menu basicamente quedar disponible y actualizar los precios*/}
+                        <div className="flex flex-col items-center justify-between ">
 
-                            {/*boton para generar informes*/}
+                            <div className="flex flex-row">
+                                {/*Boton para agregar productos */}
 
+                                <button
+                                    onClick={togglePopup} // Llama a togglePopup para mostrar el pop-up
+                                    className="flex flex-auto items-center px-2 py-2 bg-gray-600 text-white rounded space-x-4 mr-2"
+                                >
+                                    <img src={check} alt="add" />
+                                    <span>Agregar Producto</span>
+                                </button>
+
+                                {/*Boton para agregar eliminar prodcutos */}
+
+                                <button className="flex flex-auto items-center px-2 py-2 bg-gray-600 text-white rounded space-x-4 mr-2">
+                                    <img src={delete_icon} alt="edit" />
+
+                                    <span>Eliminar Producto</span>
+                                </button>
+
+                                {/*boton para actualizar un producto*/}
+
+                                <button className="flex flex-auto items-center px-2 py-2 bg-gray-600 text-white rounded space-x-4 mr-2">
+                                    <img src={update_icon} alt="edit" />
+
+                                    <span>Actualizar Producto</span>
+                                </button>
+
+                                {/*Boton para agregar el menu basicamente quedar disponible y actualizar los precios*/}
+
+                                {/*boton para generar informes*/}
+
+                                <button className="flex flex-auto items-center px-2 py-2 bg-gray-600 text-white rounded space-x-4 mr-2">
+                                    <img src={update_icon} alt="edit" />
+
+                                    <span>Generar Informe</span>
+                                </button>
+
+                            </div>
                             <Table
                                 data={opcionesP}
                                 columns={columns}
-                                filter={filterId}
-                                dataToFilter="id"
+                                filter={filternombre}
+                                dataToFilter="nombre"
                                 initialSortName="id"
                                 onSelectionChange={handleSelectionChange}
                             />
+                            {showPopup && (
+                                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+                                    <div className="bg-white p-8 rounded-lg shadow-lg w-96">
+                                        <h2 className="text-2xl font-bold mb-4">Agregar Nuevo Producto</h2>
 
+                                        <form onSubmit={handleSubmit} className="space-y-4">
+                                            {/* Campo para Nombre */}
+                                            <div>
+                                                <label className="block text-gray-700 font-bold mb-2">Nombre</label>
+                                                <input
+                                                    type="text"
+                                                    name="nombre"
+                                                    value={newProduct.nombre}
+                                                    onChange={handleChange}
+                                                    className="w-full px-4 py-2 border rounded-lg"
+                                                    required
+                                                />
+                                            </div>
+
+                                            {/* Campo para Valor */}
+                                            <div>
+                                                <label className="block text-gray-700 font-bold mb-2">Valor</label>
+                                                <input
+                                                    type="number"
+                                                    name="valor"
+                                                    value={newProduct.valor}
+                                                    onChange={handleChange}
+                                                    className="w-full px-4 py-2 border rounded-lg"
+                                                    required
+                                                />
+                                            </div>
+
+                                            {/* Campo para Stock */}
+                                            <div>
+                                                <label className="block text-gray-700 font-bold mb-2">Stock</label>
+                                                <input
+                                                    type="number"
+                                                    name="stock"
+                                                    value={newProduct.stock}
+                                                    onChange={handleChange}
+                                                    className="w-full px-4 py-2 border rounded-lg"
+                                                    required
+                                                />
+                                            </div>
+
+                                            {/* Botones para Cancelar y Agregar */}
+                                            <div className="flex justify-end space-x-4">
+                                                <button
+                                                    type="button"
+                                                    onClick={togglePopup}
+                                                    className="px-4 py-2 bg-gray-400 text-white rounded-lg"
+                                                >
+                                                    Cancelar
+                                                </button>
+                                                <button
+                                                    type="submit"
+                                                    className="px-4 py-2 bg-green-600 text-white rounded-lg"
+                                                >
+                                                    Agregar
+                                                </button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
