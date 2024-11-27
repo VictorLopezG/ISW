e75f596268d14fa99f485450b6c24a005578f740
JersonPalmaSarandona <jerson.palma2101@alumnos.ubiobio.cl>
2024-11-27 17:20:17 -0300

    fix eliminar producto

--- frontend/src/services/producto.service.js
+++ frontend/src/services/producto.service.js
@@ -7,10 +7,7 @@ export async function createProducto(data) {
         const dataRegister = convertirMinusculas(data);
         const { nombre,valor,stock, categoria} = dataRegister;
         const response = await axios.post('/producto/crearProducto', {
-            nombre:nombre,
-            valor:valor,
-            stock:stock,
-            categoria:categoria
+            nombre:nombre,valor:valor,stock:stock,categoria:categoria
 
         });
         console.log(response.data);
